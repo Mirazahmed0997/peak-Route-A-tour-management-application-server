30a72c2fdf223381aa6c1c764dc7730b669da1cb
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-10-25 01:51:34 +0600

    jwtProtectedRouteForADMIN

--- src/app/Modules/User/User.route.ts
+++ src/app/Modules/User/User.route.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response, Router } from "express";
 import { userControllers } from "./User.controller";
 import { createUserZodSchema } from "./User.validation";
 import { validateRequest } from "../../middlewares/validateRequest";
+import jwt, { JwtPayload } from "jsonwebtoken"
+import AppError from "../../errorHelper/AppError";
+import { Role } from "./User.interface";
 
 
 
@@ -13,6 +16,39 @@ const router =Router()
 
 router.post('/register',validateRequest(createUserZodSchema),userControllers.createUser)
 
-router.get('/AllUsers', userControllers.getAllUsers)
+router.get('/AllUsers',
+    async (req:Request,res:Response,next:NextFunction)=>{
+
+   try {
+     const accesToken= req.headers.authorization
+     console.log(accesToken)
+
+     if(!accesToken)
+     {
+        throw new AppError(403,"No token recieved")
+     }
+    const verifyToken=jwt.verify(accesToken,"secret")
+
+    if(!verifyToken)
+    {
+        throw new AppError(403,`Unauthorised access ${verifyToken}`)
+    }
+
+    console.log(verifyToken)
+
+    if((verifyToken as JwtPayload).role !== Role.ADMIN || Role.SUPER_ADMIN)
+    {
+         throw new AppError(403,"Permission Denied")
+    }
+
+    console.log(verifyToken)
+
+    next()
+   } catch (error) {
+        next(error)
+   }
+
+}, 
+userControllers.getAllUsers)
 
 export const userRoutes= router
