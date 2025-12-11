1aa48ea0919c2ced0465759da89761ac069fa134
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-12-11 17:23:26 +0600

    createEJSFileForForgotPasswordTemplate

--- src/app/Utils/sendEmail.ts
+++ src/app/Utils/sendEmail.ts
@@ -1,5 +1,8 @@
 import nodemailer from "nodemailer"
 import { envVars } from "../Config/env"
+import path from "path"
+import ejs from "ejs"
+import AppError from "../errorHelper/AppError"
 
 const transporter= nodemailer.createTransport({
     
@@ -15,7 +18,7 @@ const transporter= nodemailer.createTransport({
 interface SendEmailOptions{
     to:string,
     subject:string,
-    template:string,
+    templateName:string,
     templateData?:Record<string, any>
     attachments?:[{
         filename:string,
@@ -24,23 +27,32 @@ interface SendEmailOptions{
     }]
 }
 
-const sendEmail= async({
+export const sendEmail= async({
     to,
     subject,
-    template,
+    templateName,
     templateData,
     attachments
 }: SendEmailOptions)=>
 {
+   try {
+     const templatePath=path.join(__dirname, `templates/${templateName}.ejs`)
+    const html= await ejs.renderFile(templatePath,templateData)
     const info= await transporter.sendMail({
         from:envVars.EMAIL_SENDER.SMTP_FROM,
         to:to,
         subject:subject,
-        html:template,
+        html:html,
         attachments:attachments?.map(attachment=>(
             { filename:attachment.filename,
             content:attachment.content,
             contentType:attachment.contentType})),
 
     })
+
+    console.log(`${to}: ${info.messageId}`)
+   } catch (error : any) {
+    console.log("email sending error", error.message)
+    throw new AppError(401,"email error")
+   }
 }
