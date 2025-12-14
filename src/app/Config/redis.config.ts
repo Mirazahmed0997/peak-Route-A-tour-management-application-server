eb355c6e53578c349e30c59bfd2d0c09501479c5
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-12-14 18:30:49 +0600

    configureRedis

---
+++ src/app/Config/redis.config.ts
@@ -0,0 +1,27 @@
+import { createClient } from 'redis';
+import { envVars } from './env';
+
+const redisClient = createClient({
+    // username: envVars.REDIS.USERNAME,
+    // password: envVars.REDIS.PASSWORD,
+    socket: {
+        host: envVars.REDIS.REDIS_HOST,
+        port: Number(envVars.REDIS.REDIS_PORT)
+    }
+});
+
+redisClient.on('error', err => console.log('Redis Client Error', err));
+
+
+// await redis_client.set('foo', 'bar');
+// const result = await redis_client.get('foo');
+// console.log(result)  // >>> bar
+
+export const redisConnect=async()=>
+{
+    if (!redisClient.isOpen)
+    {
+        await redisClient.connect();
+        console.log("Redis Connected")
+    }
+}
