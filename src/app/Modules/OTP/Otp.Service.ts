2a724c6ce061dcd4144a62dea0d3e5b79455aa1c
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-12-15 11:50:15 +0600

    createVerifyOTPAPI

--- src/app/Modules/OTP/Otp.Service.ts
+++ src/app/Modules/OTP/Otp.Service.ts
@@ -1,6 +1,8 @@
 import crypto from "crypto"
 import { redisClient } from "../../Config/redis.config"
 import { sendEmail } from "../../Utils/sendEmail"
+import AppError from "../../errorHelper/AppError"
+import { User } from "../User/User.model"
 
 const OTP_EXPIRATION = 2 * 60
 
@@ -11,28 +13,66 @@ const generateOTP = async (length = 6) => {
 }
 
 const sendOTP = async (email: string, name: string) => {
-    const otp = await generateOTP()
-    const redisKey= `otp:${email}`
 
-    await redisClient.set(redisKey, otp,{
-        expiration:{
-            type:"EX",
-            value:OTP_EXPIRATION
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new AppError(404, 'User not found')
+    }
+    if (user.isVerified) {
+        throw new AppError(401, `${email} is Already Veryfied`)
+    }
+
+    const otp = await generateOTP()
+    const redisKey = `otp:${email}`
+
+    await redisClient.set(redisKey, otp, {
+        expiration: {
+            type: "EX",
+            value: OTP_EXPIRATION
         }
     })
 
     await sendEmail({
-        to:email,
-        subject:"Your OTP Code",
-        templateName:"otpTmeplate",
-        templateData:{
-            name:name,
-            otp:otp
+        to: email,
+        subject: "Your OTP Code",
+        templateName: "otpTmeplate",
+        templateData: {
+            name: name,
+            otp: otp
         }
     })
 
 }
-const veryfyOTP = async () => {
+const veryfyOTP = async (email: string, otp: string) => {
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new AppError(404, 'User not found')
+    }
+    if (user.isVerified) {
+        throw new AppError(401, `${email} is Already Veryfied`)
+    }
+
+    const redisKey = `otp:${email}`
+    const savedOtp = await redisClient.get(redisKey)
+
+    if (!savedOtp) {
+        throw new AppError(401, "invalid OTP")
+    }
+
+    if (savedOtp !== otp) {
+        throw new AppError(401, "invalid OTP")
+    }
+
+
+
+    await Promise.all([
+        User.updateOne({ email }, { isVerified: true }, { runValidators: true }),
+        redisClient.del([redisKey])
+    ])
 
 
     return {}
